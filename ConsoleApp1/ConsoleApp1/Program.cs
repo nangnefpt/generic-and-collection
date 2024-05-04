7a13379749b313f47f5691db6f0359ba56c93b8c
DESKTOP-RPUSVGS\Admin <tranduynang4@gmail.com>
2024-05-04 21:17:46 +0700

    generic

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+public class MyClass<T> {
+    // ví dụ về generic class
+    private T data;
+    public T Value { 
+    get => data;
+    set => data = value;
+        }
+public override string ToString() => $"Value: {data}";
+        }
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass<string> name = new MyClass<string>()
+        { Value = "Nang"};
+        Console.WriteLine(name);
+        MyClass<float> version = new MyClass<float>()
+        { Value = 5.5f};
+        Console.WriteLine(version);
+        dynamic obj = new { Id = 1, Name = "Ky" };
+        MyClass<dynamic> myClass = new MyClass<dynamic>() { Value = obj };
+        Console.WriteLine(myClass);
+        Console.ReadLine();
+
+    }
+}
